a5b628d4931c0fd65490b51b5fd230ff0402d06c
elimf <elim.florvil@icloud.com>
2022-02-17 20:36:25 +0100

    now

--- src/pages/home.php
+++ src/pages/home.php
@@ -2,48 +2,52 @@
 <?php 
 $title="Home";
 ob_start() ?>
-<div class="container-fluid">
-  <div class="row justify-content-center">
-            <div class="col-6">
-                        <h1 class="display-4"> <?php if (isset($_SESSION['user'])) {
-                        echo ("Bonjour"." ".$_SESSION['user']);
-                        }else{
-                        
-                            }?> </h1>
-            </div>
-            <div class="col-6">
-                <h2>Want to add a new picture?</h2>
-                 <a href="/Create"><button class="btn btn-primary">Add a picture</button></a>
-            </div>
-            
-            <?php 
-
-                foreach($_SESSION["idPost"] as $key => $value ) {
-                     
-
-                    ?>
-                    <div class="card-wrapper mt-5 col-lg-4 col-md-6 col-xs-12">
-                <div class="card">
-                    <div class="card-img-wrapper">
-                    <img class="card-img-top" src="<?php echo  $value['url'] ; ?>" alt="Card image capdddddddddddddd">
-                    </div>
-                    <div class="card-body ">
-                        <p><?php echo $value['likes'] ; ?> &hearts;</p>
-                        <p><?php echo $value['contenu'] ; ?></p>
-                    <div class="card-content">
-                        <p class="card-text">Comments</p>
-                        <a href="#" class="btn btn-default">Add a comment</a>
-                    </div>
-                    </div>
-                </div>
-            </div>
-               <?php }
-            ?>
-            
-  </div>
+<div class="container-fluid ">
+    <div class="row ">
+        <div class="col-sm-12 col-md-6 col-lg-10 ">
+            <h2>Want to add a new picture?</h2>
+        </div>  
+        <div class="col-sm-12 col-md-6 col-lg-2">
+             <a href="/Create"><button class="btn btn-primary">Add a picture</button></a>
+        </div> 
+    </div>
 </div>
-
-    
+<div class="container-fluid mt-0">
+        <div class="row ">
+            <div class="  col-12 ">
+                <div class="row ">
+                        <?php foreach($_SESSION["idPost"] as $key => $value ) { ?>
+                            <div class="card-wrapper  col-12 col-sm-6 col-md-3 ">
+                                    <div class="card">
+                                        <div class="card-img-wrapper">
+                                            <img
+                                                class="card-img-top"
+                                                src="<?php echo  $value['url'] ; ?>"
+                                                alt="Card image capdddddddddddddd"
+                                                
+                                            />
+                                        </div>
+                                        <div class="card-body ">
+                                            <div class="row">
+                                                <div class="col-3">
+                                                    <a href="#"><?php echo $value['likes'] ; ?> &hearts;</a>
+                                                </div>
+                                                <div class="col-9">
+                                                    <p><?php echo $value['contenu'] ; ?></p>
+                                                </div>
+                                            </div>
+                                            <div class="card-content">
+                                                <p class="card-text">Comments</p>
+                                                <a href="#" class="btn btn-warning">Add a comment</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        <?php }?> 
+                </div>
+            </div>       
+        </div>
+</div>
   
  
     <style>
@@ -79,17 +83,18 @@ ob_start() ?>
         display: inline-block;
         position: relative;
         overflow: hidden;
-        min-height: 400px;
+        max-height: 400px;
+        max-width:400px;
         height: 100%;
         }
         .card:hover {
-        box-shadow: 8px 12px 31px -10px #ab98ab;
+        box-shadow: 8px 12px 31px -10px yellow;
         }
         .card-img-wrapper {
         display: flex;
         align-items: center;
         justify-content: center;
-        height: 50%;
+        height: 100%;
         overflow: hidden;
         }
         .card-img-wrapper img {
@@ -111,7 +116,7 @@ ob_start() ?>
         bottom: 0;
         left: 0;
         right: 0;
-        height: 50%;
+        height: 75px;
         background-color: #fff;
         transition: 1.5s ease;
         }
@@ -122,17 +127,24 @@ ob_start() ?>
         width: 100%;
         height: auto;
         transition: 1.5s ease;
+        margin-top:3%;
         }
         .card:hover .card-body {
-        height: 80%;
+        height: 70%;
         }
         .card:hover .card-content {
         bottom: 0;
         }
-        body {
-        margin: 0;
-        background-image: linear-gradient(to right, #ECE9E6 , #FFFFFF);
+        .card-img-top {
+        width: 100%;
+        height:100%;
+        max-width: 400px;
+        max-height: 400px;
+        border-top-left-radius: calc(0.25rem - 1px);
+        border-top-right-radius: calc(0.25rem - 1px);
         }
+        
+       
     </style>
 <?php $content = ob_get_clean(); ?>
 
--- www/index.php
+++ www/index.php
@@ -45,12 +45,12 @@ switch ($request) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"defer ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous" defer></script>
     <script src="//code.jquery.com/jquery-2.1.4.js"></script>
-    <link rel="stylesheet" href="/src/style/style.css"/>
+
 </head>
   <style>
   #fond{
       margin-top:30px;
-      height:1000px;
+      height:auto;
   }
   </style>
 <body style="margin-top:100px;">
@@ -63,7 +63,4 @@ switch ($request) {
 } ?>
 </section>
 </body>
-<footer>
-<?php require(__DIR__ . '/../src/partials/footer.php'); ?>
-</footer>
 </html>
